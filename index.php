e8a1d847e9658cff454c44cea7efc1c328771621
Thiego Ferreira <thiegocpf@gmail.com>
2025-02-11 10:52:48 -0400

    Index e cliente.php estilizados

--- index.php
+++ index.php
@@ -3,14 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tela login</title>
+    <title>Sistema de Vendas</title>
+    <link rel="stylesheet" href="./public/assets/css/style.css">
 </head>
 <body>
-    <h2>MENU</h2><br><br>
-    
-        <a href="./App/View/cliente.php">CLIENTES</a><br>
-        <a href="./App/View/produto.php">PRODUTOS</a><br>
-        <a href="./App/View/venda.php">VENDAS</a><br>
+    <div class="titulo-container">
+        <h2 class="titulo">Sistema de Vendas Capivara Futurista</h2>
+    </div>
 
+    <div class="container">
+        <div class="botao"><a href="./App/View/cliente.php">CLIENTES</a></div>
+        <div class="botao"><a href="./App/View/produto.php">PRODUTOS</a></div>
+        <div class="botao"><a href="./App/View/venda.php">VENDAS</a></div>
+    </div>
 </body>
 </html>
